e896eb97168f5cadf4f892ab7e92deba03bd481f
Lukmanul Hakim <jurpro.online@gmail.com>
2022-04-14 20:45:37 +0700

    Final Project
    Final Project - Reviewed by Dicoding

--- app/src/main/java/com/ujanglukmanbdg/githubtoday/MainActivity.kt
+++ app/src/main/java/com/ujanglukmanbdg/githubtoday/MainActivity.kt
@@ -1,26 +1,94 @@
 package com.ujanglukmanbdg.githubtoday
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.ujanglukmanbdg.githubtoday.databinding.ActivityMainBinding
+import com.ujanglukmanbdg.githubtoday.ui.themes.SettingPreferences
+import com.ujanglukmanbdg.githubtoday.ui.themes.ThemeViewModel
+import com.ujanglukmanbdg.githubtoday.ui.themes.ThemeViewModelFactory
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    private var profileImageUrl = "https://avatars.githubusercontent.com/u/5201127?v=4"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val btnMoveUserList: Button = findViewById(R.id.btn_move_user_list)
-        btnMoveUserList.setOnClickListener(this)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btn_move_user_list -> {
-                val moveIntent = Intent(this@MainActivity, ListUserActivity::class.java)
-                startActivity(moveIntent)
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Jika ada pertanyaan, silakan hubungi kami", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        binding.apply {
+            Glide.with(this@MainActivity)
+                .load(profileImageUrl)
+                .circleCrop()
+                .into(binding.navView.getHeaderView(0)
+                    .findViewById(R.id.imageView_profile_menu))
+        }
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.aboutMeActivity,
+                R.id.nav_home,
+                R.id.nav_gallery,
+                R.id.nav_flight,
+                R.id.nav_themeActivity,
+                R.id.nav_flight
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+        val pref = SettingPreferences.getInstance(dataStore)
+        val themeViewModel = ViewModelProvider(this, ThemeViewModelFactory(pref))[ThemeViewModel::class.java]
+
+        themeViewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
